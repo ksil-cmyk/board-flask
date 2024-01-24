df3feb7bc9c530c719c5810ed5a33ebfe920ec21
ksil_windows <ldw0811@gmail.com>
2024-01-24 23:28:18 +0900

    v2.05

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
+
+from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -10,4 +13,4 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    return 'Pybo index'
+    return redirect(url_for('question._list'))
